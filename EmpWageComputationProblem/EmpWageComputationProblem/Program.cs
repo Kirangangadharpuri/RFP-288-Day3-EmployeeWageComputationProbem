478ba4c6f108fbbec4847d32d306a9d8c8184fd5
“Kirangangadharpuri” <kiranpuri532@gmail.com>
2023-06-16 17:28:39 +0530

    [Kiran]Add:// Check employee is present or not

---
+++ EmpWageComputationProblem/EmpWageComputationProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage Computation Problem");
+            UC1_EmployeeAttendence.EmployeeCheck();
+        }
+    }
+}
